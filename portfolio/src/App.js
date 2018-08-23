bb7791e9256dcc54102e69768dd560eafa2daf9e
Cristina Laryea <Number2@Georges-MacBook-Air.local>
2018-08-23 15:24:45 -0400

    created menu file, added img

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,19 +2,15 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import Header from '../src/Components/Header';
+import Menu from '../src/Components/Menu';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Menu />
       <Header />
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p> */}
+  
       </div>
     );
   }
--- portfolio/src/Components/Header.js
+++ portfolio/src/Components/Header.js
@@ -3,35 +3,22 @@ import { Link } from 'react-router-dom';
 import '../Css/Header.css';
 
 
+
 class Header extends React.Component {
     render() {
         return (
             <div>
-                <div class="navbar">
-                <ul>
-                    <li><a class="active" href="#home">Home</a></li>
-                    <li><a href="#news">News</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                    <li><a href="#about">About</a></li>
-                </ul>
-                </div>
-                <div className="background"></div>
-                <div className="background2x">
-                <div className="col justify-content-left topHeader"></div>
-                <Link to="/">
-                <img src={require('../Assets/IMG_0014.jpg')} alt='Cristina Laryea'/>
+                
+                <div className="halfBackground">
+                <img src={require('../Assets/flowers.jpg')} alt='flowers' />
+               
+                
+                
                 <h1>CRISTINA LARYEA</h1>
                 <h3>FULL STACK DEVELOPER</h3>
-                </Link>
                 
-                {/* <div className="Links justiy-content">
-                    <Link to='/About'>ABOUT</Link>
-                    <Link to='/Projects'>PROJECTS</Link>
-                    <Link to='/Blog'>BLOG</Link>
-                    <Link to='/Life'>LIFE</Link>
-                    <Link to='/Contact'>CONTACT</Link>
-                </div> */}
-                <div className="Header-text px-5"></div>
+                
+                <div className="Header-text px-5">
                 <h1 className="mb-5 Intro">Hello Everyone!</h1>
                     <p className="summary">
                     Let me begin by telling you my story, I worked as a Pharmacy Technician for thirteen years. I enjoyed the interaction with patients, and
@@ -42,11 +29,10 @@ class Header extends React.Component {
                     The best part about programming that I enjoy is problem solving because there is always a solution. 
                     For instance, thinking meticulously and making a plan that will lead to a working solution. 
                     Lambda school opened a whole new world of opportunities for me. I learned different programming languages, along with different frameworks that have taught me how to create a front and back-end websites and applications. 
-                    
                     </p>
-                <hr />
-            </div>
-            </div>
+                    </div>
+             </div>
+           </div>
         );
     }
 }
@@ -54,3 +40,4 @@ class Header extends React.Component {
 
 
 export default Header;
+
