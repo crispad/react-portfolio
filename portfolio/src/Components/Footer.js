f6f6c6aa242c841db32b2359553375463e4382a8
Cristina Laryea <Number2@Georges-MacBook-Air.local>
2018-08-24 18:25:23 -0400

    added copyright on bottom

--- portfolio/src/Components/Footer.js
+++ portfolio/src/Components/Footer.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class Footer extends Component {
     render() {
         return (
+            <div>
             <div className="footer row">
                 <div className="col-lg-12">
                 <a href="mailto:claryea01@gmail.com"></a><i class="fa fa-envelope-o">Email</i>
@@ -12,7 +13,13 @@ class Footer extends Component {
                 </div>
 
             </div>
-        )
+            <div className="row" id="copyright" style={{margin: 0}}>
+            <div className="col-lg-12">
+            <p className="small"> 2018 Cristina Laryea</p>
+            </div>
+            </div>
+            </div>  
+        );
     }
 }
 
