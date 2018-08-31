66b9f154523f6de28afb870abfc8044277131a51
Cristina Laryea <crispad06@gmail.com>
2018-08-31 13:05:57 -0400

    contact form updated styling, and tested to work when pressed submit

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,32 +1,79 @@
 import React, { Component } from 'react';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import styled from 'styled-components';
 
+const FormDiv = styled.div`
+  margin: 0 auto;
+  width: 50%;
+`
+
+const FormLabel = styled.label`
+  display: block;
+`
+
+const FormInput = styled.input`
+  width: 100%;
+`
+
+const CommentInput = styled.textarea`
+  width: 100%;
+  height: 6em;
+`
+
+const EmailMe = styled.p`
+  text-align: center;
+  padding: 4%;
+`
 
 
 
 class Contact extends Component {
     render() {
         return (
-            <div>
-                <h1>Get In Touch With Me!</h1>
-            <Form>
-                <FormGroup>
-                    <Label for="Name">Name</Label>
-                    <Input type="name" name="name" id="name" placeholder="Enter name here" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="Email">Email</Label>
-                    <Input type="email" name="email" id="email" placeholder="Enter email here" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="Comment">Comment</Label>
-                    <Input type="comment" name="comment" id="comment" />
-                </FormGroup>
-               <Button>Submit</Button> 
-            </Form>
+            // <div>
+            //     <h1>Get In Touch With Me!</h1>
+            // <Form>
+            //     <FormGroup>
+            //         <Label for="Name">Name</Label>
+            //         <Input type="name" name="name" id="name" placeholder="Enter name here" />
+            //     </FormGroup>
+            //     <FormGroup>
+            //         <Label for="Email">Email</Label>
+            //         <Input type="email" name="email" id="email" placeholder="Enter email here" />
+            //     </FormGroup>
+            //     <FormGroup>
+            //         <Label for="Comment">Comment</Label>
+            //         <Input type="comment" name="comment" id="comment" />
+            //     </FormGroup>
+            //    <Button>Submit</Button> 
+            // </Form>
 
-            Or, just email me directly at <a href="mailto:claryea01@gmail.com"><strong>claryea01@gmail.com</strong></a>.
+            // Or, just email me directly at <a href="mailto:claryea01@gmail.com"><strong>claryea01@gmail.com</strong></a>.
+            // </div>
+            <div>
+        {/* <CenteredHeader>Get in touch with me!</CenteredHeader> */}
+
+        <FormDiv>
+          <form action="https://formspree.io/claryea01@gmail.com" method="POST" id="contactform">
+            <div>
+              <FormLabel>Name:</FormLabel>
+              <FormInput type="text" name="name" />
             </div>
+            <div>
+              <FormLabel>Email:</FormLabel>
+              <FormInput type="text" name="_replyto" />
+            </div>
+            <div>
+              <FormLabel>Comment:</FormLabel>
+              <CommentInput name="comment" />
+            </div>
+            <button type="submit">Submit</button>
+          </form>
+        </FormDiv>
+
+        <EmailMe>Or, just email me directly at <a href="mailto:claryea01@gmail.com" target="_blank"><strong>claryea01@gmail.com</strong></a>.</EmailMe>
+
+      </div>
+
         )
     }
 }
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,40 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
-// class SimpleMenu extends Component {
-//   render() {
-//     return (
-      // <div className="Menu">
-      //   <ul>
-      //     <Link to = '/about me'><span className='text-dark'>About Me</span></Link>
-      //     <Link to = '/projects'><span className='text-dark'>Projects</span></Link>
-      //     <Link to = '/blogs'><span className='text-dark'>Blogs</span></Link>
-      //     <Link to = '/contact'><span className='text-dark'>Contact</span></Link>
-          
 
-      //   </ul>
-      // </div>
-//       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-//   <div class="collapse navbar-collapse" id="navbarNav">
-//     <ul class="navbar-nav">
-//       <li class="nav-item active">
-//         <a class="nav-link" href="/Home">Home <span class="sr-only"></span></a>
-//       </li>
-//       <li class="nav-item">
-//         <a class="nav-link" href="/About Me">About Me</a>
-//       </li>
-//       <li class="nav-item">
-//         <a class="nav-link" href="/Projects">Projects</a>
-//       </li>
-//       <li class="nav-item">
-//         <a class="nav-link disabled" href="/Blogs">Blogs</a>
-//       </li>
-//       <li class="nav-item">
-//         <a class="nav-link disabled" href="/Contacts">Contact</a>
-//       </li>
-//     </ul>
-//   </div>
-// </nav>
+
+
 
 class SimpleMenu extends React.Component {
   constructor(props) {
@@ -78,9 +47,7 @@ class SimpleMenu extends React.Component {
     );
   }
 }
-//     )
-//   }
-// }
+
 
 
 export default SimpleMenu;
