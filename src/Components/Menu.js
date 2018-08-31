8f3d3e76a835bb22f07de73e3e0836605a96ff1e
Cristina Laryea <Number2@Georges-MacBook-Air.local>
2018-08-31 12:26:08 -0400

    nav bar-wip

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,60 +1,9 @@
 import React, { Component } from 'react';
-//import { Link } from 'react-router-dom';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
-
-// import Button from '@material-ui/core/Button';
-// import Menu from '@material-ui/core/Menu';
-// import MenuItem from '@material-ui/core/MenuItem';
-// import { NavLink} from 'react-router-dom';
-// import '../Css/Menu.css'
-
-// class SimpleMenu extends React.Component {
-//   state = {
-//     anchorEl: null,
-//   };
-
-//   handleClick = event => {
-//     this.setState({ anchorEl: event.currentTarget });
-//   };
-
-//   handleClose = () => {
-//     this.setState({ anchorEl: null });
-//   };
-
+// class SimpleMenu extends Component {
 //   render() {
-//     const { anchorEl } = this.state;
-
 //     return (
-//       <div> 
-//         <Button
-//           aria-owns={anchorEl ? 'simple-menu' : null}
-//           aria-haspopup="true"
-//           onClick={this.handleClick}
-//         >
-//           Explore
-//         </Button>
-//         <div className="menu">
-//         <Menu
-//           id="simple-menu"
-//           anchorEl={anchorEl}
-//           open={Boolean(anchorEl)}
-//           onClose={this.handleClose}
-//         >
-
-//           <MenuItem onClick={this.handleClose}>About Me</MenuItem>
-//           <MenuItem onClick={this.handleClose}>Projects</MenuItem>
-//           <MenuItem onClick={this.handleClose}>Blogs</MenuItem>
-//           <MenuItem onClick={this.handleClose}>Contact</MenuItem>
-//         </Menu>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-class SimpleMenu extends Component {
-  render() {
-    return (
       // <div className="Menu">
       //   <ul>
       //     <Link to = '/about me'><span className='text-dark'>About Me</span></Link>
@@ -65,34 +14,73 @@ class SimpleMenu extends Component {
 
       //   </ul>
       // </div>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  {/* <a class="navbar-brand" href="#">Cristina Laryea</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button> */}
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="/Home">Home <span class="sr-only"></span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/About Me">About Me</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/Projects">Projects</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="/Blogs">Blogs</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="/Contacts">Contact</a>
-      </li>
-    </ul>
-  </div>
-</nav>
-    )
+//       <nav class="navbar navbar-expand-lg navbar-light bg-light">
+//   <div class="collapse navbar-collapse" id="navbarNav">
+//     <ul class="navbar-nav">
+//       <li class="nav-item active">
+//         <a class="nav-link" href="/Home">Home <span class="sr-only"></span></a>
+//       </li>
+//       <li class="nav-item">
+//         <a class="nav-link" href="/About Me">About Me</a>
+//       </li>
+//       <li class="nav-item">
+//         <a class="nav-link" href="/Projects">Projects</a>
+//       </li>
+//       <li class="nav-item">
+//         <a class="nav-link disabled" href="/Blogs">Blogs</a>
+//       </li>
+//       <li class="nav-item">
+//         <a class="nav-link disabled" href="/Contacts">Contact</a>
+//       </li>
+//     </ul>
+//   </div>
+// </nav>
+
+class SimpleMenu extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="faded" light>
+          <NavbarBrand href="/" className="mr-auto">Cristina Laryea</NavbarBrand>
+          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+          <Collapse isOpen={!this.state.collapsed} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink href="/About Me/">About Me</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/Projects/">Projects</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/Blogs/">Blogs</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/Contact/">Contact</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
   }
 }
+//     )
+//   }
+// }
 
 
 export default SimpleMenu;
