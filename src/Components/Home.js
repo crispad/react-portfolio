65742bec0d0ce099c55cfdb8709fc4654b0703f6
Cristina Laryea <Number2@Georges-MacBook-Air.local>
2018-09-04 18:03:02 -0400

    wip on background image

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,28 +1,30 @@
 import React from 'react';
-
-import '../Css/Home.css';
+import { Image } from 'react-bootstrap';
+//import '../Css/Home.css';
 
 
 
 class Home extends React.Component {
     render() {
+
+        let background = {backgroundSize : 'cover'};
+        let textStyle = {
+            position: 'absolute',
+            top: '50%',
+            left: '50%'
+        };
         return (
-            <div>
+            <div style={{width: 'auto'}}>
+                <Image  
+                    style={background} responsive
+                    src='../Assets/IMG_0014.jpg'>
+                </Image>
+                <h1 style={textStyle}>Cristina Laryea</h1>
                 
 
-                <div className="background">
-                <div className="title"></div>
-                
-                <h1>Cristina Laryea <span class="badge badge-secondary"></span></h1>
-       
-                <h2>Full Stack Developer<span class="badge badge-secondary"></span></h2>
                 
                 
-                <div className="Header-text px-5">
                 
-                    
-                    </div>
-             </div>
            </div>
           
         );
