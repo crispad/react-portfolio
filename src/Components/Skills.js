559a4a21b1d65b8122bd0a3cce8a2a2377888c97
Cristina Laryea <crispad06@gmail.com>
2018-10-23 23:14:10 -0400

    added card for skills-wip with styling

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -10,48 +10,83 @@ class Skills extends React.Component {
 
     
 
-    <div class="card-deck">
-  <div class="card">
-    <div class="card-body">
-      <h5 class="card-title">Front-end:</h5>
-      <ul class="card-text"><li>JavaScript (ES6)</li>
-                  <li>React</li>
-                  <li>Redux</li>
-                  <li>HTML5</li>
-                  <li>CSS3/Sass</li>
-                  <li>jQuery</li>
-                  <li>AJAX</li>
-                  <li>Python</li>
-                  <li>Material-UI</li></ul>
+//     <div class="card-deck">
+//   <div class="card">
+//     <div class="card-body">
+//       <h5 class="card-title">Front-end:</h5>
+//       <ul class="card-text"><li>JavaScript (ES6)</li>
+//                   <li>React</li>
+//                   <li>Redux</li>
+//                   <li>HTML5</li>
+//                   <li>CSS3/Sass</li>
+//                   <li>jQuery</li>
+//                   <li>AJAX</li>
+//                   <li>Python</li>
+//                   <li>Material-UI</li></ul>
       
-    </div>
-  </div>
+//     </div>
+//   </div>
   
-  <div class="card">
-    <div class="card-body">
-      <h5 class="card-title">Back-end:</h5>
-      <ul class="card-text"><li>Node.js</li>
-                            <li>Express</li>
-                            <li>SQL/PostgreSQL</li>
-                            <li>MongoDB</li>
-                            <li>Django</li></ul>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-body">
-      <h5 class="card-title">Other:</h5>
-      <ul class="card-text"><li>Git</li>
-                  <li>Webpack</li>
-                  <li>JSON</li>
-                  <li>Mocha/Chai Testing</li>
-                  <li>Agile Development</li>
-          </ul>
-    </div>
-  </div>
+//   <div class="card">
+//     <div class="card-body">
+//       <h5 class="card-title">Back-end:</h5>
+//       <ul class="card-text"><li>Node.js</li>
+//                             <li>Express</li>
+//                             <li>SQL/PostgreSQL</li>
+//                             <li>MongoDB</li>
+//                             <li>Django</li></ul>
+//     </div>
+//   </div>
+//   <div class="card">
+//     <div class="card-body">
+//       <h5 class="card-title">Other:</h5>
+//       <ul class="card-text"><li>Git</li>
+//                   <li>Webpack</li>
+//                   <li>JSON</li>
+//                   <li>Mocha/Chai Testing</li>
+//                   <li>Agile Development</li>
+//           </ul>
+//     </div>
+//   </div>
+// </div>
+   <div> 
+{/* // <div id="js-flip-1" class="flip">
+//     <div class="card">
+//         <div class="face front">Front End</div>
+//         <div class="face back">Back</div>
+//     </div>
+// </div>
+
+// <div id="js-flip-2" class="flip">
+//     <div class="card">
+//         <div class="face front">Back End</div>
+//         <div class="face back">Back</div>
+//     </div>
+// </div>
+
+// <div id="js-flip-3" class="flip">
+//     <div class="card">
+//         <div class="face front">Other</div>
+//         <div class="face back">Back</div>
+//     </div>
+</div> */}
+
+<div class="maincontainer">
+
+<div class="thecard">
+
+  <div class="thefront"><h1>Front End</h1><p>This is the front of the card. It contains important information. Please see overleaf for more details.</p></div>
+
+  <div class="theback"><h1>List of Tech</h1><p>Your use of this site is subject to the terms and conditions governing this and all transactions.</p>
+ </div>
+
 </div>
-    
+</div>
+</div>
+
+
         )
     }
-}
+  }
 
 export default Skills;
